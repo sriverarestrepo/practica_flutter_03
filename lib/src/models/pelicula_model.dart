d82617ef2f85389a3c6b6292a54d21b512c9a26d
santrire <srivera@ias.com.co>
2020-06-19 21:55:11 -0500

    Se crea el modelo de pelicula

---
+++ lib/src/models/pelicula_model.dart
@@ -0,0 +1,74 @@
+
+class Peliculas {
+
+  List<Pelicula> items = new List();
+
+  Peliculas();
+
+  Peliculas.fromJsonList( List<dynamic> jsonList){
+
+    if(jsonList == null ) return;
+
+    for (var item in jsonList) {
+      final pelicula = new Pelicula.fromJsonMap(item);
+      items.add(pelicula);
+    }
+
+  }
+
+}
+
+
+
+class Pelicula {
+
+  double popularity;
+  int voteCount;
+  bool video;
+  String posterPath;
+  int id;
+  bool adult;
+  String backdropPath;
+  String originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String title;
+  double voteAverage;
+  String overview;
+  String releaseDate;
+
+  Pelicula({
+    this.popularity,
+    this.voteCount,
+    this.video,
+    this.posterPath,
+    this.id,
+    this.adult,
+    this.backdropPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.title,
+    this.voteAverage,
+    this.overview,
+    this.releaseDate,
+  });
+
+  Pelicula.fromJsonMap( Map<String, dynamic> jsonMap){
+    popularity       = jsonMap['popularity'] / 1;
+    voteCount        = jsonMap['vote_count'];
+    video            = jsonMap['video'];
+    posterPath       = jsonMap['poster_path'];
+    id               = jsonMap['id'];
+    adult            = jsonMap['adult'];
+    backdropPath     = jsonMap['backdrop_path'];
+    originalLanguage = jsonMap['original_language'];
+    originalTitle    = jsonMap['original_title'];
+    genreIds         = jsonMap['genre_ids'].cast<int>();
+    title            = jsonMap['title'];
+    voteAverage      = jsonMap['vote_average'] / 1;
+    overview         = jsonMap['overview'];
+    releaseDate      = jsonMap['release_date'];
+  }
+}
+
