697e36f92e5e8681f8ae569e151f29a09a5fd883
santrire <srivera@ias.com.co>
2020-06-19 20:54:57 -0500

    Creacion de card swiper de base

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:pelis_app_practica/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pelis App'),
+        backgroundColor: Colors.indigo,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){}
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas(),
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],);
+  }
+}
