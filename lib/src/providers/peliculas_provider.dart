2712325639a1ed58b2698f794be44771c08d118c
santrire <srivera@ias.com.co>
2020-06-21 15:22:03 -0500

    Ajustando inicial patron Bloc

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 import 'package:pelis_app_practica/src/models/pelicula_model.dart';
 
 class PeliculasProvider {
@@ -7,6 +8,17 @@ class PeliculasProvider {
   String _apiKey    = '6a1526ecbd7ae0ad00a01a496677c88b';
   String _url       = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+
+  final _popularesStream = StreamController();
+
+  void disposeStreams(){
+    _popularesStream?.close();
+  }
+
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
     final respuesta = await http.get(url);
@@ -29,12 +41,16 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+
+    _popularesPage++;
+
     final url = Uri.https(
       _url,
       '3/movie/popular',
       {
         'api_key'   : _apiKey,
-        'language'  : _language
+        'language'  : _language,
+        'page'      : _popularesPage.toString(),
     });
 
     return await _procesarRespuesta(url);
